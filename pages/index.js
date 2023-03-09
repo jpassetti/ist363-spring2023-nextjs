fe35b798b2d2c39be682dbf3ecc09186ec32f992
Jeff Passetti <jpassetti@Jeffs-MacBook-Air.local>
2023-03-09 16:48:51 -0500

    My code after Tuesday 3/7/23 class

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,44 @@
 import Button from '../components/Button';
 import Header from '../components/Header';
+import Image from 'next/image';
 import Layout from '../components/Layout';
 
-const Homepage = () => {
+import { getAllVehicles } from '../lib/api';
+
+export async function getStaticProps() {
+  // Get external data from the file system, API, DB, etc.
+  const data = getAllVehicles();
+
+  // The value of the `props` key will be
+  //  passed to the `Home` component
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+const Homepage = ({data}) => {
+  console.log({data});
   return <Layout>
     <h1>Homepage</h1>
     <Button 
       label="Register now" 
       type="primary" 
     />
-    <Button 
-      label="Download now" 
-      type="secondary"
-    />
-    <Button 
-      label="Learn more" 
-      type="primary"
-    />
-    <Button 
-      label="Buy now" 
-      type="secondary" 
-    />
+    {data.map((car) => {
+        const { model, slug } = car;
+        return <article>
+          <Image 
+            src={`/vehicles/${slug}/medium.webp`}
+            alt={`${model} car`}
+            width={350}
+            height={185}
+          />
+          <h2>{model}</h2>
+        </article>
+    })}
+   
   </Layout>
 }
 export default Homepage
