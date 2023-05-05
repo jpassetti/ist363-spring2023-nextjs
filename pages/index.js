7869a3b5aded3d857eef662eae60121c82274933
Jeff Passetti <jpassetti@gmail.com>
2023-05-05 11:22:39 -0400

    useEffect added to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,17 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 import Layout from '../components/Layout';
 
-import { 
-  //getAllVehicles, 
-  //getAllColors 
-} from '../lib/api';
+const Homepage = () => {
+  const router = useRouter();
 
-export async function getStaticProps() {
-  // Get external data from the file system, API, DB, etc.
-  //const data = getAllVehicles();
-  //const colors = getAllColors();
+  useEffect(() => {
+    router.push('/vehicles');
+  }, []);
 
-  // The value of the `props` key will be
-  //  passed to the `Home` component
-  return {
-    props: {
-      //data,
-      //colors
-    }
-  }
-}
-
-const Homepage = ({ 
-  //data, 
-  //colors
-}) => {
-  //console.log({data});
   return <Layout>
-    {
-      //<Showcase />
-     //<LocationPicker />
-     //<CarColorPicker colors={colors} />
-     //<SectionCTA />
-    }
+   
   </Layout>
 }
 export default Homepage
